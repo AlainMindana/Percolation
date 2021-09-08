ceb62ae3043467f40a523fde88e7b206aee505e7
Alain Mindana <alainmindana@gmail.com>
2021-09-09 00:51:44 +0800

    This is working

--- src/com/example/vins/Percolation.java
+++ src/com/example/vins/Percolation.java
@@ -1,55 +1,45 @@
 package com.example.vins;
 
 public class Percolation {
-    public static int size = 3;
+    public static int size = 5;
     private static WQUPC wqupc = new WQUPC(size);
     public static int matrixSize = wqupc.matrix[0].length; //
     public static int j = 0;
     public static int k = 0;
 
     public static void main(String[] args) {
-        //Open sites here
-//        open(2);
-//        open(0);
-        for (int i = 0; i < 4; i++){
-            open((int) (Math.random()*(8 + 1) + 0));
+
+        for (int i = 0; i < 15; i++){
+            open((int) (Math.random()*(24 + 1) + 0));
+            percolates();
         }
 
-        //Get the latest open virtual top site at row 0 for virtual top connection
-        while (!isOpen(0,j)){
-            ++j;
-            if (j == matrixSize) break;
-            System.out.println(j);
-        }
 
-        //Connect the virtual top to all open Row[0] elements
-        while (k < matrixSize -1){
-            wqupc.union(j, k);
-            k++;
-        }
-
-//        System.out.println(isFull(2,0));
-//        isFull(1,0);
-//        for (int i = 0; i < 5; i++){
-//            isFull((int) (Math.random()*(4 - 0 + 1) + 0),(int) (Math.random()*(4 - 0 + 1) + 0));
-//        }
-        for (int i = 0; i < size; i++){
-            for (int f = 0; f < size; f++){
-                isFull(i,f);
+        do{
+            if (!isOpen(0, j)){
+                ++j;
             }
-        }
-        System.out.println(percolates());
 
- /*       for (int i = 0; i < wqupc.id.length; i++) {
+            wqupc.union(j,k);
+            k++;
+        }while (k < matrixSize-1 && j < matrixSize-1);
+
+        for (int i = 0; i < wqupc.id.length; i++) {
             System.out.println(i + " : " + wqupc.root(i));
-        }*/
+        }
+
         //Display matrix
         for (int i = 0; i < wqupc.matrix.length; i++){
-            for (int j = 0; j < wqupc.matrix.length; j++){
-                System.out.print(wqupc.matrix[i][j] + "\t");
+            for (int n = 0; n < wqupc.matrix.length; n++){
+                if (wqupc.id[size * i + n] == wqupc.root(size * i + n) && wqupc.matrix[i][n] != -2){
+                    wqupc.matrix[i][n] = wqupc.root(size * i + n);
+                }
+                System.out.print(wqupc.matrix[i][n] + "\t");
             }
             System.out.println();
         }
+
+        System.out.println(percolates());
     }
 
 
@@ -57,7 +47,10 @@ public class Percolation {
         if (n > wqupc.id.length - 1){
             throw new IllegalArgumentException();
         }
+
         wqupc.matrix[n/wqupc.matrix.length][n%matrixSize] = -1;
+
+        isFull(n/wqupc.matrix.length, n%matrixSize);
     }
 
     public static  boolean isOpen(int row, int col){
@@ -84,15 +77,6 @@ public class Percolation {
             throw new IllegalArgumentException();
         }
         if (isOpen(row,col)){
-            if (row + 1 <= matrixSize - 1 && isOpen(row+1, col)){
-                wqupc.union(wqupc.id[size * row + col], wqupc.id[size * (row+1) + col]);
-            } if ((row - 1 <= matrixSize - 1 && row-1 >= 0) && isOpen(row-1, col)){
-                wqupc.union(wqupc.id[size * row + col], wqupc.id[size * (row-1) + col]);
-            } if (col + 1 <= matrixSize - 1 && isOpen(row, col+1)){
-                wqupc.union(wqupc.id[size * row + col], wqupc.id[size * row + (col+1)]);
-            } if ((col - 1 <= matrixSize - 1 && col-1 >= 0) && isOpen(row, col-1)){
-                wqupc.union(wqupc.id[size * row + col], wqupc.id[size * row + (col-1)]);
-            }
             if (wqupc.root(wqupc.id[size * 0 + j]) == wqupc.root(wqupc.root(wqupc.id[size * row + col]))) return true;
         }
         return false;
@@ -124,13 +108,8 @@ public class Percolation {
             x++;
         }
 
+        if (isFull(matrixSize - 1, d)) return true;
 
-        if (wqupc.root(wqupc.id[size * 0 + j]) == wqupc.root(wqupc.id[size * (matrixSize-1) + d])){
-//            System.out.println(wqupc.root(wqupc.id[size * 0 + j]));
-//            System.out.println(wqupc.root(wqupc.id[size * (matrixSize-1) + d]));
-
-            return true;
-        }
         return false;
     }
 }
